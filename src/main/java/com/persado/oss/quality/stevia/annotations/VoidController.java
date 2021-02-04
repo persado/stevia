77e429c5228f6efd1cbc153b47db91a85ef322fd
Ioannis Chitiris <ioannis.chitiris@persado.com>
2021-02-04 11:55:36 +0200

    add proxy capability/ selenoid add sshJUtils for ssh

--- src/main/java/com/persado/oss/quality/stevia/annotations/VoidController.java
+++ src/main/java/com/persado/oss/quality/stevia/annotations/VoidController.java
@@ -536,6 +536,11 @@ class VoidController implements WebController {
 	}
 
 	@Override
+	public String getTableContainsTextHeaderPosition(String locator, String headerName) {
+		return null;
+	}
+
+	@Override
 	public String getTableElementColumnPosition(String locator,
 			String elementName) {
 		// TODO Auto-generated method stub
